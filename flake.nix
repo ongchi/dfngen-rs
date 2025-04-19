dd051d7f557c846b60aab18bed49c95fa9f06f6f
Chih Wang <ongchi@users.noreply.github.com>
2025-04-19 08:48:53 +0800

    Add cargo-audit to flake.nix

--- flake.nix
+++ flake.nix
@@ -6,6 +6,7 @@
     };
     flake-utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    rust-overlay.url = "github:oxalica/rust-overlay";
   };
 
   outputs = {
@@ -13,15 +14,20 @@
     fenix,
     flake-utils,
     nixpkgs,
+    rust-overlay,
   }:
     flake-utils.lib.eachDefaultSystem (system: let
       toolchain = fenix.packages.${system}.stable.toolchain;
-      pkgs = nixpkgs.legacyPackages.${system};
+      overlays = [ (import rust-overlay) ];
+      pkgs = import nixpkgs {
+        inherit system overlays;
+      };
     in {
       devShells.default = pkgs.mkShell {
         buildInputs = [
           toolchain
           pkgs.libiconv
+          pkgs.cargo-audit
         ];
       };
     });
